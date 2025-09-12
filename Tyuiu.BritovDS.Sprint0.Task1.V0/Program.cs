c80aaac82578aa72e0cbf37b2704d267b88cbace
user <user@WIN-FJ5S0KDD7IV>
2025-09-12 19:18:01 +0300

    Add project files.

---
+++ Tyuiu.BritovDS.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.BritovDS.Sprint0.Task1.V0
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "Daniil";
+            int age = 18;
+            Console.WriteLine("Hello, my name is " + name + " i am " + age + " years old");
+            Console.ReadKey();
+
+        }
+    }
+}

00968312277bacfb4878b234841f9b16ac76056a
user <user@WIN-FJ5S0KDD7IV>
2025-09-14 20:16:29 +0300

    Task4

---
+++ Tyuiu.BritovDS.Sprint0.Task4.V0.Test/DataServiceTest.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.BritovDS.Sprint0.Task4.V0.Lib;
+
+namespace Tyuiu.BritovDS.Sprint0.Task4.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        public DataServiceTest()
+        {
+        }
+
+        [TestMethod]
+        public void CheckAdditionValid()
+        {
+            Assert.AreEqual(10, DataService.Addition(5, 5));
+        }
+
+        [TestMethod]
+        public void CheckSubtractionValid()
+        {
+            Assert.AreEqual(5, DataService.Subtruction(10, 5));
+        }
+
+        [TestMethod]
+        public void CheckMultiplicationValid()
+        {
+            Assert.AreEqual(50, DataService.Multiplication(10, 5));
+        }
+
+        [TestMethod]
+        public void CheckDivisionValid()
+        {
+            Assert.AreEqual(3, DataService.Division(9, 3));
+        }
+    }
+}

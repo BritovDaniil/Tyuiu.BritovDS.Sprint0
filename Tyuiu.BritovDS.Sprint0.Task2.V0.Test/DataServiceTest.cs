3edc1325afdb2ceea02f14658c710cbf967b5dc3
user <user@WIN-FJ5S0KDD7IV>
2025-09-14 19:00:15 +0300

    Task2

---
+++ Tyuiu.BritovDS.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+using Tyuiu.BritovDS.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.BritovDS.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Даниил";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет, Даниил", res);
+        }
+        
+    }
+}
---
+++ Tyuiu.BritovDS.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.BritovDS.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.BritovDS.Sprint0.Task2.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Даниил"));
+            Console.ReadKey();
+        }
+    }
+}
